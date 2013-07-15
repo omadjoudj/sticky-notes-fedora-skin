b7de7085bc4e525e8d4f7c32309804f4bba18560
Athmane Madjoudj <athmane@fedoraproject.org>
2013-07-16 00:41:43 +0100

    Fix cookie issue with Firefox

--- fedora/js/main.js
+++ fedora/js/main.js
@@ -59,27 +59,27 @@ $(document).ready(function() {
     }, 100);
     
     // Fetch author and language values from cookies
-    var author = $.cookie('stickynotes_author');
-    var language = $.cookie('stickynotes_language');
-    var index = -1;
+    //var author = $.cookie('stickynotes_author');
+    //var language = $.cookie('stickynotes_language');
+    //var index = -1;
 
-    if (author != null) {
-        $('#paste_user').val(author);
-    }
+    //if (author != null) {
+    //    $('#paste_user').val(author);
+    //}
 
-    for (i = 1; i <= 10; i++) {
-        var $option = $('#paste_lang option:nth-child(' + i.toString() + ')');
-        
-        if ($option.attr('value') == language) {
-            index = i - 1;
-        }
-    }
+    //for (i = 1; i <= 10; i++) {
+    //    var $option = $('#paste_lang option:nth-child(' + i.toString() + ')');
+    //    
+    //    if ($option.attr('value') == language) {
+    //        index = i - 1;
+    //    }
+    //}
 
-    if (language != null && index < 0) {
-        $('#paste_lang').val(language);
-    } else if (language != null) {
-        $('#paste_lang').get(0).selectedIndex = index;
-    }
+    //if (language != null && index < 0) {
+    //    $('#paste_lang').val(language);
+    //} else if (language != null) {
+    //    $('#paste_lang').get(0).selectedIndex = index;
+    //}
     
     // Insert tab in the code box
     $('#paste_data').keydown(function (e) {      
